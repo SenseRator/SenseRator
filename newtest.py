157bf8090ea9d5a5b0cc61ecd197d642b9b5ef65
GabyShamblin <gabyshamblin@gmail.com>
2023-09-14 15:22:24 -0400

    Comments

--- newtest.py
+++ newtest.py
@@ -10,15 +10,19 @@ print("Use SimpleGUI with point cloud window")
 app = o3d.visualization.gui.Application.instance
 app.initialize()
 
+# Import sample cloud
 # sample_ply_data = o3d.data.PLYPointCloud()
 # pcd = o3d.io.read_point_cloud(sample_ply_data.path)
 
+# Import cloud
 point_cloud = o3d.io.read_point_cloud("Hotel.ply")
 
+# For SimpleUI
 layout = [[sui.Text("Hello, world"), sui.Text(size=(15,1), key='-TEXT-')],
           [sui.Text("This is a text box"), sui.InputText()],
 					[sui.Submit(), sui.Cancel()]]
 
+# Show test options window
 def simpleui(vis):
 	window = sui.Window("More Options", layout)
 
@@ -26,6 +30,7 @@ def simpleui(vis):
 		event, values = window.read()
 		print(event, values)
 		print('Something happend', values[0])
+		
 		if event == sui.WIN_CLOSED or event == 'Cancel':
 			break
 		if event == 'Submit':
@@ -33,14 +38,17 @@ def simpleui(vis):
 
 	window.close()
 
+# Create point cloud window
 vis = o3d.visualization.O3DVisualizer("O3DVis", 700, 500)
 vis.add_action("More Options", simpleui)
 vis.add_geometry("Hotel", point_cloud)
 vis.reset_camera_to_default()
 
+# Show window
 app.add_window(vis)
 app.run()
 
+# Normal visualizer (no skybox or menu)
 # main_vis = o3d.visualization.Visualizer()
 # main_vis.create_window("main_vis", 700, 500)
 # main_vis.add_geometry(point_cloud)
